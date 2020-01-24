2d166f4b3c67dbe5f04626f5fdd076aec6e85b0b
Justin C Williams <45780143+justinwilly@users.noreply.github.com>
2020-01-24 13:48:47 -0500

    mvp complete

---
+++ data/seeds/001-projects.js
@@ -0,0 +1,16 @@
+
+exports.seed = function(knex) {
+  // Inserts seed entries
+  return knex("projects").insert([
+    {
+      name: "Test Project 1",
+      description: "Build a test project",
+      completed: false
+    },
+    {
+      name: "Test Project 2",
+      description: "Build a another project",
+      completed: false
+    }
+  ]);
+};
